04245062f45f0a3d9aa13ec5be4d4716af965d6e
Atom <atomcraft.4u@web.de>
2024-11-21 22:52:11 +0100

    small change

--- UserInterface.java
+++
@@ -1,5 +0,0 @@
-public final class UserInterface{  
-    public static void main(String[] args){
-        System.out.println("lul");
-    }
-}
--- demo/src/main/java/com/example/UserInterface.java
+++ demo/src/main/java/com/example/UserInterface.java
@@ -1,8 +1,11 @@
-package demo;
+package com.example;
+
+import javafx.stage.Stage;
 
 public final class UserInterface{
     
     public static void main(String[] args){
         System.out.println("Hi");
+        App a = new App();
     }
 }
