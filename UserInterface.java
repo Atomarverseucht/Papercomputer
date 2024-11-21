a128ea8402b592e60d543248315b882c72852c90
Atom <atomcraft.4u@web.de>
2024-11-21 22:38:44 +0100

    added javafx standard classes

--- UserInterface.java
+++ UserInterface.java
@@ -1,6 +1,5 @@
-public final class UserInterface{
-    
+public final class UserInterface{  
     public static void main(String[] args){
-        System.out.println("Hi");
+        System.out.println("lul");
     }
 }
---
+++ demo/UserInterface.java
@@ -0,0 +1,8 @@
+package demo;
+
+public final class UserInterface{
+    
+    public static void main(String[] args){
+        System.out.println("Hi");
+    }
+}
