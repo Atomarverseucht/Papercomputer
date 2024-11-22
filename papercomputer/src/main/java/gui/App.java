69c32bb676b3a9b1f467fcbc10d04eeab2c5b4ac
Atom <atomcraft.4u@web.de>
2024-11-22 13:35:50 +0100

    better structure

--- papercomputer/src/main/java/gui/App.java
+++ papercomputer/src/main/java/gui/App.java
@@ -1,4 +1,4 @@
-package com.example;
+package gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- papercomputer/src/main/java/gui/PrimaryController.java
+++ papercomputer/src/main/java/gui/PrimaryController.java
@@ -1,4 +1,4 @@
-package com.example;
+package gui;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
