8324a0f13339b0b0558e889a1aaead77d7465d48
Atom <atomcraft.4u@web.de>
2024-11-25 20:12:07 +0100

    better save

--- papercomputer/src/main/java/gui/PrimaryController.java
+++ papercomputer/src/main/java/gui/PrimaryController.java
@@ -1,19 +1,27 @@
 package gui;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
+import logic.memory;
 
-public class PrimaryController {
+public class PrimaryController implements Initializable{
 
     @FXML
     private TextArea textarea;
     @FXML
     private void switchToExecuteView() throws IOException {
         String s = textarea.getText();
-        System.out.println(s);
         logic.memory.setCommand(s);
-        App.setRoot("secondary");
-        
+        App.setRoot("secondary"); 
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        textarea.setText(memory.getCommandString());
+
     }
 }
--- papercomputer/src/main/java/gui/SecondaryController.java
+++ papercomputer/src/main/java/gui/SecondaryController.java
@@ -1,12 +1,24 @@
 package gui;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
+import java.net.URL;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import logic.memory;
 
-public class SecondaryController {
+public class SecondaryController implements Initializable{
 
+    @FXML
+    Label lbOutput;
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        lbOutput.setText(memory.getCommandString());
+
     }
 }
